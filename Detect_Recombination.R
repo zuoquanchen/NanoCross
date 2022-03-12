dbe13f6e95c5f26bbaed79b261ab692a5a9716bc
zuoquanchen <55975575+zuoquanchen@users.noreply.github.com>
2022-03-12 09:30:30 +0800

    Update Detect_Recombination.R

--- Detect_Recombination.R
+++ Detect_Recombination.R
@@ -19,7 +19,7 @@ blockgap=function (vec){
 	block_left=c()
 	block_right=c()
 	for (b in vec){
-	dat_tmp=hap[hap$V7==b,]
+	dat_tmp=hap[hap$block==b,]
 	tmpnum=nrow(dat_tmp)
 	b_left=dat_tmp$V2[1]
 	b_right=dat_tmp$V2[tmpnum]
@@ -29,8 +29,8 @@ blockgap=function (vec){
 	blockdata=data.frame(block_left,block_right)
 	return(blockdata)
 }
-
-findRecReads<- function(i,seq_num=0,m=0,gapin="false"){
+#Main program
+FindRecReads<- function(i,seq_num=0,m=0,gapin="false"){
 	rec=c()
 	pos_strati=as.numeric(pos[i])
 	seq_loc= pos_strati
@@ -38,25 +38,24 @@ findRecReads<- function(i,seq_num=0,m=0,gapin="false"){
 	cigar_vec=strsplit(cigar[i],split ="(?=[A-Za-z])(?<=[0-9])|(?=[0-9])(?<=[A-Za-z])", perl=TRUE)
 	#Iterate through CIGAR Get location information
 	for (ci in cigar_vec[[1]]){
-	m=m+1
-	if (jishu(m)){
-	  num=as.numeric(ci)
-	}else {
-	  if (ci=="M"){
-		seq_loc=seq_loc+num
-		seq_num=seq_num+num
-	  }else if(ci=="I"){
-		seq_loc=seq_loc
-		seq_num=seq_num+num
-	  }else if(ci=="D"){
-		seq_loc=seq_loc+num
-	  }else if(ci=="S"){
-		seq_num=seq_num+num
-	  }else if(ci=="H"){}
-		}
+		m=m+1
+		if (jishu(m)){
+			num=as.numeric(ci)
+		}else {
+			if (ci=="M"){
+				seq_loc=seq_loc+num
+				seq_num=seq_num+num
+			}else if(ci=="I"){
+				seq_loc=seq_loc
+				seq_num=seq_num+num
+			}else if(ci=="D"){
+				seq_loc=seq_loc+num
+			}else if(ci=="S"){
+			seq_num=seq_num+num
+			}else if(ci=="H"){}
+		}	
 	}
-
-	#Filtering reference span shorter than 5000bp reads 
+	#Filtering reference span short 
 	pos_end=seq_loc-1
 	pos_vec=hapPos[which(hapPos >pos_strati & hapPos < pos_end)]
 	blockdata3=blockdata[blockdata$block_left<=pos_end & blockdata$block_right >=pos_strati,]
@@ -70,17 +69,14 @@ findRecReads<- function(i,seq_num=0,m=0,gapin="false"){
 		gapin="true"
 		npos=0
 	}
-	
 	vcf_have="true"
 	if (npos < 13){
 	vcf_have="false"
 	}
-  #phase molecules 
+    #phase molecules 
 	if (vcf_have=="true" & gapin=="false"){
-		reads_geno=c()
-		m=0
+		seq_num=0;m=0;reads_geno=c()
 		seq_loc=as.numeric(pos[i])
-		seq_num=0
 		for (loc in pec_vec){
 			while (seq_loc < loc) {
 				m=m+1
@@ -111,9 +107,8 @@ findRecReads<- function(i,seq_num=0,m=0,gapin="false"){
 			}
 			reads_geno=append(reads_geno,geno)
 		}
-		
-		hap_A=as.character(hap_part$V8)
-		hap_B=as.character(hap_part$V9)
+		hap_A=as.character(hap_part$hap0)
+		hap_B=as.character(hap_part$hap1)
 		hap_vec=c()
 		for (p in 1:npos){
 			if (as.character(reads_geno[p]) ==as.character(hap_A[p])){
@@ -129,77 +124,65 @@ findRecReads<- function(i,seq_num=0,m=0,gapin="false"){
 				hap_vec=append(hap_vec,hap_p)
 			}
 		}
-		hap_f=hap_vec[1:5]
+		#print(hap_vec)
+		hap_f=hap_vec[1:7]
 		hap_f=hap_f[hap_f !="NA"]
 		hap_f=append(hap_f,"Hap")
-		hap_e=hap_vec[(npos-4):npos]
+		hap_e=hap_vec[(npos-6):npos]
 		hap_e=hap_e[hap_e !="NA"]
 		hap_e=append(hap_e,"Hap")
 		#Using the sliding window method to determine if reads are recombinant molecules
-		if (mainHap(hap_f)==mainHap(hap_e)){
-		}else{
+		if (mainHap(hap_f)!=mainHap(hap_e)){
 			phase_vec=c()
 			phase_pos=c()
-			id_vec=c()
 			for (n in 4:(npos-3)){
-				aa=hap_vec[n-3]
-				bb=hap_vec[n-2]
-				cc=hap_vec[n-1]
-				dd=hap_vec[n]
-				ee=hap_vec[n+1]
-				ff=hap_vec[n+2]
-				abcd=c(aa,bb,cc,dd,ee,ff)
-				abcd=as.data.frame(table(abcd))
-				rownames(abcd)=abcd$abcd
-				if (abcd$abcd[1]=="A"){
-					Anum=as.numeric(abcd$Freq[1])
-				}else if (abcd$abcd[1]!="A"){
+			
+			win_vec=hap_vec[(n-3):(n+3)]
+			win_vec=win_vec[win_vec != "NA"]
+			win_data=as.data.frame(table(win_vec))
+			rownames(win_data)=win_data$win_vec
+			if (nrow(win_data)==1){
+				if (win_data$win_vec[1]=="A"){
+					Anum=as.numeric(win_data$Freq[1])
+					Bnum=0
+				}else {
+					Bnum=as.numeric(win_data$Freq[1])
 					Anum=0
 				}
-				if (nrow(abcd)==1){
-					if (abcd$abcd[1]=="B"){
-						Bnum=as.numeric(abcd$Freq[1])
-					}else {
-						Bnum=0
-					}
-				}
-				if (nrow(abcd)==2){
-					if (abcd$abcd[2]=="B"){
-						Bnum=as.numeric(abcd$Freq[2])
-					}else {
-					Bnum=0
-					}
-				}
-				if (nrow(abcd)==3){
-					if (abcd$abcd[2]=="B"){
-						Bnum=as.numeric(abcd$Freq[2])
-					}else {
-						Bnum=0
-					}
-				}
-				if (Anum >=6){
+			}else if (nrow(win_data)==2){
+				Anum=as.numeric(win_data$Freq[1])
+				Bnum=as.numeric(win_data$Freq[2])
+			}else {
+				Anum=0
+				Bnum=0
+			}
+			
+			if (length(win_vec)==0){
+				phase="NA"
+			}
+			else{
+				if ((Anum/length(win_vec))>=(5/6)){
 					phase="A"
-				}else if (Bnum >=6){
+				}else if ((Bnum/length(win_vec))>=(5/6)){
 					phase="B"
 				}else {
 					phase="NA"
 				}
-				phase_pos=append(phase_pos,pos_vec[n])
-				id_vec=append(id_vec,n)
-				phase_vec=append(phase_vec,phase)
 			}
-			phase_data=data.frame(phase_vec,phase_pos)
+			phase_pos=append(phase_pos,pos_vec[n])
+			phase_vec=append(phase_vec,phase)
+	        }
+	        phase_data=data.frame(phase_vec,phase_pos)
 			phase_data=phase_data[!phase_data$phase_vec %in% c("NA") ,]
-			le=nrow(phase_data)
-			if (le >=3 & mainHap(hap_f)==as.character(phase_data$phase_vec[1])){
-				print(phase_data$phase_vec)
-				print(hap_vec)
-				for(n in 2:(length(phase_data[,1])-1)){
-					aaa=phase_data$phase_vec[n-1]
-					bbb=phase_data$phase_vec[n]
-					if (phase_data$phase_vec[1]!= phase_data$phase_vec[le] ){
-						if (aaa!=bbb){
+			phase_num=nrow(phase_data)
+			if (phase_num >=3 & mainHap(hap_f)==as.character(phase_data$phase_vec[1])){
+				for(n in 2:phase_num){
+					a=phase_data$phase_vec[n-1]
+					b=phase_data$phase_vec[n]
+					if (phase_data$phase_vec[1]!= phase_data$phase_vec[phase_num] ){
+						if (a!=b){
 							rec=c(i,qname[i],as.character(phase_data$phase_pos[n-1]),as.character(phase_data$phase_pos[n]))
+							#print(rec)
 						}
 					} 
 				}
@@ -208,19 +191,15 @@ findRecReads<- function(i,seq_num=0,m=0,gapin="false"){
 	}
 	return(rec)
 }
-
-#### Arguments and input ####
 args<-commandArgs(TRUE)
-if(length(args)!=6){
+if(length(args)!=5){
   stop("Usage: Rscript Detect_Recombination.R
        <1. Input Bam files (Same as used for call variation files)  >
        <2. Input phase block file (output file of whatsphase) >
        <3. Number of threads >
 	   <4. Chromsome (For one chromosome only) >
-       <5. output filestem (output with columns Index, Reads, Pos_left, pos_right. For one chromosome only.) >
-       ",call=F)
+       <5. output filestem (output with columns Index, Reads, Pos_left, pos_right. For one chromosome only.) > ",call=F)   
 }
-
 bamFile <- args[1]
 phaseFile <- args[2]
 threads <- as.numeric(args[3])
@@ -228,24 +207,23 @@ chromosome <- args[4]
 outFile <- args[5]
 
 #Read data & Data preparation
-data<-scanBam(bamFile) 
-hap=read.table(phasefile)
+data<-scanBam(bamFile)
+hap=read.table(phaseFile,header=T)
+hapPos=as.numeric(hap$V2)
 # Do each chromosome
 bam=data[[1]][data[[1]]$rname == chromosome]
-cigar=as.character(bam[[1]]$cigar)
-seq=as.character(bam[[1]]$seq)
-qname=as.character(bam[[1]]$qname)
-pos=as.numeric(bam[[1]]$pos)
-blockdata=blockgap(unique(hap$V7))
-#run findRecReads function
+cigar=as.character(bam$cigar)
+seq=as.character(bam$seq)
+qname=as.character(bam$qname)
+pos=as.numeric(bam$pos)
+blockdata=blockgap(unique(hap$block))
+#run FindRecReads function parallel
 cl <- makeCluster(threads) #not to overload your computer
 registerDoParallel(cl)
 recdata <- foreach(i=1:length(qname), .combine=rbind) %dopar% {
-	recom=findRecReads(i)
+	recom=FindRecReads(i)
 	recom
 }
 stopCluster(cl) #stop cluster
 # Save
-write.table(recdata,file=outFile,quote=F,col.names=T,row.names=F)
-
-
+write.table(recdata,file=outFile,quote=F,col.names=F,row.names=F)
